c2369fd583157532f56719b348df9c7089119889
s_abe <s_abe@s-abenoMacBook-Air.local>
2023-08-09 16:49:28 +0900

    バリデーションを実装

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 20}
+  validates :startdate, presence: true
+  validates :enddate, presence: true
+  validates :memo, length: {maximum: 500}
+  validate :enddate_must_be_after_startdate
+
+  def enddate_must_be_after_startdate
+    if enddate.present? && enddate < startdate
+      errors.add(:enddate, "は開始日以降の日付である必要があります")
+    end
+  end
 end
